047f85dee3aac17861ae09b8a191bef8f7956414
Florent Jaby <florent.jaby@gmail.com>
2024-08-21 01:27:57 +0200

    optional grain

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
 module.exports = function (config) {
   config.addPassthroughCopy('css/*.css')
+  config.addPassthroughCopy('images/*.svg')
+  config.addPassthroughCopy('images/*.png')
+  config.addPassthroughCopy('images/*.jpg')
 }
